e252d83edc5a864b7ae50d6e27a0b2a076a6cfac
Arthur-SS <arthursantossoares31@gmail.com>
2021-05-30 21:38:03 -0300

    Adicionado botão para jogar e reiniciar o jogo

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Criando o jogo da cobrinha para versão de desktop é mobile. Este projeto tem p
 
 # 🕹Snake Game
 Creating the snake game for the desktop and mobile version. This project aims to study JavaScript properties with HTML and CSS. A challenge proposed by DIO in the bootCamp HTML Web Developer. 
+
+
+[Teste o jogo](https://arthur-ss.github.io/JogoCobrinha/)
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var jogando = document.querySelector(".btaJogar")
+jogando.style.display = "flex";
+
 let canvas = document.getElementById("cobrinha"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d"); //....
 let box = 32;
@@ -13,36 +16,11 @@ let comida = {
 };
 var fome = { x: -2, y: -2 };
 
-const imgMaca = document.getElementById("maca")
+const imgMaca = document.getElementById("maca") //Imagens de estilização 
 var corpoCobra = document.getElementById("corpoCobra")
 const cabecaCobra = document.getElementById("cabecaCobra")
 const ossos = document.getElementById("morreCobra")
 const imgFome = document.getElementById("fome-10")
-
-let score = document.getElementById("pontos");
-var ponto = 0;
-score.innerHTML = ponto;
-let disparaFome = 100;
-
-
-function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo usando x e y e a largura e altura setadas
-};
-
-function criarCobrinha() {
-    context.drawImage(cabecaCobra, snake[0].x, snake[0].y)
-    for (i = 1; i < snake.length; i++) {
-        context.drawImage(corpoCobra, snake[i].x, snake[i].y);
-    }
-};
-
-function loop() {
-    if (snake[0].x >= 16 * box) snake[0].x = 0;
-    if (snake[0].x <= -1) snake[0].x = 16 * box;
-    if (snake[0].y >= 16 * box) snake[0].y = 0;
-    if (snake[0].y <= -1) snake[0].y = 16 * box;
-}
 
 
 document.addEventListener('keyup', setas)/* Adiciona o movimento ao personagem */
@@ -71,116 +49,154 @@ function botaoEsquerda() {
     if (esquerda == esquerda && direcao != "direita") direcao = "esquerda";
 }
 
+function jogoComeçar() {        //Função para add botão de jogar e reiniciar
+    jogando.style.display = "none";
+    snake.length = 1
+    corpoCobra = document.getElementById("corpoCobra")
 
-function desenharComida() {
-    context.drawImage(imgMaca, comida.x, comida.y);
-}
-
-function calcularPontos() {
-    ponto = ponto + 10
-    score.innerHTML = ponto
-}
-function pontoCai() {
-    ponto = ponto - 10
-    score.innerHTML = ponto
-}
-function desenharFome() {
-    context.drawImage(imgFome, fome.x, fome.y)
-}
-var xyAleatorio = Math.floor(Math.random() * 15 + 1) * box;
+    let score = document.getElementById("pontos"); //Pontuação
+    var ponto = 0;
+    const textP = "Pontos: ";
+    score.innerHTML = textP + ponto;
+    let disparaFome = 100;
 
 
-function iniciarJogo() {
+    function criarBG() {
+        context.fillStyle = "lightgreen";
+        context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo usando x e y e a largura e altura setadas
+    };
 
-    for (i = 1; i < snake.length; i++) {
-        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
-            corpoCobra = ossos
-            var perdeu = document.querySelector(".perdeu");
-            perdeu.style.display = "flex";
-            let textHTML = ""
-            textHTML += `!Você Perdeu! <div class="calculoF">seus pontos: ${ponto}</div>`
-            perdeu.innerHTML = textHTML
-            clearInterval(jogo)
+    function criarCobrinha() {
+        context.drawImage(cabecaCobra, snake[0].x, snake[0].y)
+        for (i = 1; i < snake.length; i++) {
+            context.drawImage(corpoCobra, snake[i].x, snake[i].y);
         }
     };
-    loop();
-    criarBG();
-    criarCobrinha();
-    desenharComida();
 
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
-
-    if (direcao == "direita") snakeX += box;
-    if (direcao == "esquerda") snakeX -= box;
-    if (direcao == "cima") snakeY -= box;
-    if (direcao == "baixo") snakeY += box;
-
-    if (snakeX != comida.x || snakeY != comida.y) {
-        snake.pop();
-
-    } else {
-        comida.x = Math.floor(Math.random() * 15 + 1) * box;
-        comida.y = Math.floor(Math.random() * 15 + 1) * box;
-
-        calcularPontos();
-    };
-
-    /*  Controle Fome  */
-    switch (ponto) {
-        case disparaFome:
-            fome.x = xyAleatorio;
-            fome.y = xyAleatorio;
-            break;
-        default:
-            break;
-    };
-
-    if (ponto >= disparaFome) {
-        desenharFome();
+    function loop() {
+        if (snake[0].x >= 16 * box) snake[0].x = 0;
+        if (snake[0].x <= -1) snake[0].x = 16 * box;
+        if (snake[0].y >= 16 * box) snake[0].y = 0;
+        if (snake[0].y <= -1) snake[0].y = 16 * box;
     }
-    if (snakeX != fome.x || snakeY != fome.y) {
 
-    } else {
-        snake.length = snake.length - 1;
-        fome.x = Math.floor(Math.random() * 15 + 1) * box;
-        fome.y = Math.floor(Math.random() * 15 + 1) * box;
-        pontoCai();
+
+    function desenharComida() {
+        context.drawImage(imgMaca, comida.x, comida.y);
+    }
+
+    function calcularPontos() {
+
+        ponto = ponto + 10;
+        score.innerHTML = textP + ponto;
+    }
+    function pontoCai() {
+        ponto = ponto - 10
+        score.innerHTML = ponto
+    }
+    function desenharFome() {
+        context.drawImage(imgFome, fome.x, fome.y)
+    }
+    var xyAleatorio = Math.floor(Math.random() * 15 + 1) * box;
+
+
+    var perdeu = document.querySelector(".perdeu");
+    perdeu.style.display = "none";
+
+    function iniciarJogo() {
+
+        for (i = 1; i < snake.length; i++) {
+            if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+                corpoCobra = ossos
+                var perdeu = document.querySelector(".perdeu");
+                perdeu.style.display = "flex";
+                let textHTML = ""
+                textHTML += `!Você Perdeu! <div class="calculoF">seus pontos: ${ponto}</div>`
+                perdeu.innerHTML = textHTML
+                clearInterval(jogo)
+                jogando.style.display = "block";
+                jogando.value = "reiniciar";
+            }
+        };
+
+
+        loop();
+        criarBG();
+        criarCobrinha();
+        desenharComida();
+
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+
+        if (direcao == "direita") snakeX += box;
+        if (direcao == "esquerda") snakeX -= box;
+        if (direcao == "cima") snakeY -= box;
+        if (direcao == "baixo") snakeY += box;
+
+        if (snakeX != comida.x || snakeY != comida.y) {
+            snake.pop();
+
+        } else {
+            comida.x = Math.floor(Math.random() * 15 + 1) * box;
+            comida.y = Math.floor(Math.random() * 15 + 1) * box;
+
+            calcularPontos();
+        };
+
+        /*  Controle Fome  */
+        switch (ponto) {
+            case disparaFome:
+                fome.x = xyAleatorio;
+                fome.y = xyAleatorio;
+                break;
+            default:
+                break;
+        };
+
+        if (ponto >= disparaFome) {
+            desenharFome();
+        }
+        if (snakeX != fome.x || snakeY != fome.y) {
+
+        } else {
+            snake.length = snake.length - 1;
+            fome.x = Math.floor(Math.random() * 15 + 1) * box;
+            fome.y = Math.floor(Math.random() * 15 + 1) * box;
+            pontoCai();
+        };
+
+
+        var nCabeca = {
+            x: snakeX,
+            y: snakeY,
+        };
+
+        snake.unshift(nCabeca)
+
+        /* Controle de velocidade */
+        if (snake.length == 4) {
+            clearInterval(jogo);
+            jogo = setInterval(iniciarJogo, 185)
+        } if (snake.length == 8) {
+            clearInterval(jogo);
+            jogo = setInterval(iniciarJogo, 170)
+        } if (snake.length == 10) {
+            clearInterval(jogo);
+            jogo = setInterval(iniciarJogo, 160)
+        } if (snake.length == 15) {
+            clearInterval(jogo);
+            jogo = setInterval(iniciarJogo, 140)
+        } if (snake.length == 25) {
+            clearInterval(jogo);
+            jogo = setInterval(iniciarJogo, 110)
+        } if (snake.length == 30) {
+            clearInterval(jogo);
+            jogo = setInterval(iniciarJogo, 100)
+        };
+
     };
 
 
-    var nCabeca = {
-        x: snakeX,
-        y: snakeY,
-    };
-
-    snake.unshift(nCabeca)
-
-    /* Controle de velocidade */
-    if (snake.length == 4) {
-        clearInterval(jogo);
-        jogo = setInterval(iniciarJogo, 185)
-    } if (snake.length == 8) {
-        clearInterval(jogo);
-        jogo = setInterval(iniciarJogo, 170)
-    } if (snake.length == 10) {
-        clearInterval(jogo);
-        jogo = setInterval(iniciarJogo, 160)
-    } if (snake.length == 15) {
-        clearInterval(jogo);
-        jogo = setInterval(iniciarJogo, 140)
-    } if (snake.length == 25) {
-        clearInterval(jogo);
-        jogo = setInterval(iniciarJogo, 110)
-    } if (snake.length == 30) {
-        clearInterval(jogo);
-        jogo = setInterval(iniciarJogo, 100)
-    };
-
-};
-
-
-var jogo = setInterval(iniciarJogo, 200);
-jogo;
-
-
+    var jogo = setInterval(iniciarJogo, 200);
+    jogo;
+}
